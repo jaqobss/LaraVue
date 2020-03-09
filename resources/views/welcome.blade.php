f29f49945b1373eb9343d4aaac4af94c83c3c3a1
jaqobss <sarunas.nemeiksis@gmail.com>
2020-03-09 21:31:03 +0200

    new_branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.Laravel =  { csrfToken: "{{ csrf_token() }}" }</script>
-        <title>Larticles app</title>
+        <title>Larticles app!!</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         
         <!-- Fonts -->
